96190a00e0773fcb95478e13d38e656b10d6b08e
Mirlan0077 <mirlan.0077m@gmail.com>
2024-06-27 12:12:59 +0200

    Добавление детального описания задач и страницы детального просмотра

--- tasks/models.py
+++ tasks/models.py
@@ -13,3 +13,15 @@ class Task(models.Model):
 
     def __str__(self):
         return self.description
+
+
+
+    from django.db import models
+
+    class Task(models.Model):
+        title = models.CharField(max_length=200)
+        completed = models.BooleanField(default=False)
+        description = models.TextField(blank=True)  # новое поле для подробного описания
+
+        def __str__(self):
+            return self.title
--- tasks/urls.py
+++ tasks/urls.py
@@ -4,4 +4,16 @@ from . import views
 urlpatterns = [
     path('', views.task_list, name='task_list'),
     path('add/', views.add_task, name='add_task'),
+    path('', views.index, name='index'),
+    path('task/<int:task_id>/', views.task_detail, name='task_detail'),
 ]
+
+
+from django.shortcuts import render, get_object_or_404
+from .models import Task
+
+def task_detail(request, task_id):
+    task = get_object_or_404(Task, pk=task_id)
+    return render(request, 'tasks/task_detail.html', {'task': task})
+
+
