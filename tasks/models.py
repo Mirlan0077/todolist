a4b1881cf7101ed8e07a7b9f9d15e652695ceb0f
Mirlan0077 <mirlan.0077m@gmail.com>
2024-07-15 14:05:00 +0200

    Создал в models.py  функции кастомных валидаторов

--- tasks/models.py
+++ tasks/models.py
@@ -1,17 +1,37 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
+
+STATUS_CHOICES = [
+    ('todo', 'To Do'),
+    ('in_progress', 'In Progress'),
+    ('done', 'Done'),
+]
+
+TYPE_CHOICES = [
+    ('bug', 'Bug'),
+    ('feature', 'Feature'),
+]
+
+
+def validate_no_special_characters(value):
+    if not value.isalnum():
+        raise ValidationError(_('Поле должно быть буквенно-цифровым'), params={'value': value})
+
+
+def validate_description_length(value):
+    if len(value) < 10:
+        raise ValidationError(_('Длина описания должна составлять не менее 10 символов'))
+
 
 class Task(models.Model):
-    STATUS_CHOICES = [
-        ('new', 'Новая'),
-        ('in_progress', 'В процессе'),
-        ('done', 'Сделано'),
-    ]
-
-    description = models.CharField(max_length=255)
-    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='new')
-    due_date = models.DateField(null=True, blank=True)
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=255)
+    description = models.TextField(validators=[validate_description_length])
     completed = models.BooleanField(default=False)
-    def __str__(self):
-        return self.description
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='todo')
+    task_type = models.CharField(max_length=20, choices=TYPE_CHOICES, default='feature')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return self.title
